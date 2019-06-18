238f454ae0224fd5af3062e9245853b4d34a5255
Gabe Hargrave <gkhargrave@gmail.com>
2019-06-18 06:17:49 -0700

    Setup rendering context

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -1,6 +1,11 @@
 export default class Game {
   private running = false
   private lastUpdate = 0
+  private ctx: CanvasRenderingContext2D
+
+  constructor(ctx: CanvasRenderingContext2D) {
+    this.ctx = ctx
+  }
 
   mainLoop = () => {
     const now = Date.now()
@@ -8,7 +13,7 @@ export default class Game {
     this.lastUpdate = now
 
     this.update(dt)
-    this.render(dt)
+    this.render(this.ctx, dt)
 
     if (this.running) {
       requestAnimationFrame(this.mainLoop)
@@ -30,7 +35,9 @@ export default class Game {
     console.log(`Game -> update(): ${dt}`)
   }
 
-  render(dt: number) {
+  render(ctx: CanvasRenderingContext2D, dt: number) {
     console.log(`Game -> render(): ${dt}`)
+    ctx.fillStyle = 'rgb(255, 0, 0)'
+    ctx.fillRect(10, 10, 24, 24)
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Game } from './core'
 
 const canvas = document.getElementsByTagName('canvas')[0]
+const ctx = canvas.getContext('2d')
 canvas.style.backgroundColor = 'white'
 
-const game = new Game()
+const game = new Game(ctx)
 
 window.game = game
 window.go = () => game.start()
