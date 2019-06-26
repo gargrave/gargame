9ca3dc6f3e3e4dcc58d141e838ae66bcef99ef5d
Gabe Hargrave <gkhargrave@gmail.com>
2019-06-26 15:52:42 -0700

    Add current scene to Globals

--- src/engine/Globals.ts
+++ src/engine/Globals.ts
@@ -1,4 +1,5 @@
 import { Game } from './core/Game'
+import { Scene } from './core/Scene'
 import { Keyboard } from './input/Keyboard'
 import { Log } from './utils/Log'
 
@@ -7,6 +8,7 @@ const ENV = process.env.NODE_ENV
 let _debug = false
 let _game: Game
 let _input: Keyboard
+let _scene: Scene
 
 export class Globals {
   static get debug() { return _debug } // prettier-ignore
@@ -25,6 +27,9 @@ export class Globals {
 
   static get game() { return _game } // prettier-ignore
   static set game(game: Game) { _game = game } // prettier-ignore
+
+  static get scene() { return _scene } // prettier-ignore
+  static set scene(scene: Scene) { _scene = scene } // prettier-ignore
 
   static get input() { return _input } // prettier-ignore
   static set input(input: Keyboard) { _input = input } // prettier-ignore
