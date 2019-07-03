ab62472c2fdb3231d69a5474976894062a606a31
Gabe Hargrave <gkhargrave@gmail.com>
2019-07-03 12:52:41 -0700

    Add ability to update Sprite in WithSprite

--- src/engine/behaviors/WithSprite.ts
+++ src/engine/behaviors/WithSprite.ts
@@ -3,11 +3,16 @@ import { Sprite } from '../rendering/Sprite'
 import { BaseBehavior } from './BaseBehavior'
 
 export class WithSprite extends BaseBehavior {
-  constructor(host: GameObject, private sprite: Sprite) {
+  private _sprite: Sprite
+
+  set sprite(sprite: Sprite) { this._sprite = sprite } // prettier-ignore
+
+  constructor(host: GameObject, sprite: Sprite) {
     super(host)
+    this._sprite = sprite
   }
 
   public draw(ctx: CanvasRenderingContext2D) {
-    this.sprite.draw(ctx)
+    this._sprite.draw(ctx)
   }
 }
