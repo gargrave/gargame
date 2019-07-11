13ba3e0e9c0d3f45fa9cec5de9feeec1dd435df5
Gabe Hargrave <gkhargrave@gmail.com>
2019-07-10 17:51:59 -0700

    Add handling for per-frame callbacks in Animations

--- src/rendering/Animation.spec.ts
+++ src/rendering/Animation.spec.ts
@@ -1,9 +1,69 @@
-import { Animation } from './Animation'
+import { Animation, AnimationProps } from './Animation'
+import { Entity } from '../core/Entity'
+import { Texture } from '../resources/Texture'
+
+class TestEntity extends Entity {
+  constructor(config = {}) {
+    super(config)
+  }
+}
+
+let anim: Animation
 
 describe('Animation', () => {
-  describe('Frame callbacks', () => {
-    it.todo('calls all callbacks on the correct frames')
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
 
-    it.todo('safely ignores out-of-range indices')
+  describe('Frame callbacks', () => {
+    let callbacks
+
+    beforeEach(() => {
+      callbacks = {
+        0: jest.fn(),
+        1: jest.fn(),
+        3: jest.fn(),
+        9001: jest.fn(), // just to test that invalid frames are safely ignored
+        start: jest.fn(),
+      }
+
+      const props: AnimationProps = {
+        callbacks,
+        firstFrame: 0,
+        frameDuration: 1,
+        height: 0,
+        lastFrame: 4,
+        texture: new Texture(''),
+        width: 0,
+      }
+
+      anim = new Animation(new TestEntity(), props)
+    })
+
+    it('calls all callbacks on the correct frames on every pass', () => {
+      anim.update(1)
+      anim.update(1)
+      anim.update(1)
+
+      expect(callbacks[1]).toHaveBeenCalledTimes(1)
+      expect(callbacks[1]).toHaveBeenCalledWith(1)
+      expect(callbacks[3]).toHaveBeenCalledTimes(1)
+      expect(callbacks[3]).toHaveBeenCalledWith(3)
+
+      anim.update(1)
+      anim.update(1)
+      anim.update(1)
+      anim.update(1)
+      anim.update(1)
+
+      expect(callbacks[1]).toHaveBeenCalledTimes(2)
+      expect(callbacks[3]).toHaveBeenCalledTimes(2)
+    })
+
+    it('calls a special "start" callback when animation starts', () => {
+      anim.start()
+      expect(callbacks['start']).toHaveBeenCalledTimes(1)
+      expect(callbacks['start']).toHaveBeenCalledWith('start')
+    })
   })
 })
--- src/rendering/Animation.ts
+++ src/rendering/Animation.ts
@@ -1,4 +1,4 @@
-import { mergeWhereDefined, wrap } from '@gargrave/growbag'
+import { get, mergeWhereDefined, wrap } from '@gargrave/growbag'
 
 import { Entity } from '../core/Entity'
 import { Drawable } from '../interfaces/Drawable'
@@ -8,7 +8,8 @@ import { Texture } from '../resources/Texture'
 import { Sprite } from './Sprite'
 
 export type AnimationCallbacks = {
-  [key: number]: () => void
+  start?: (frame: number) => void
+  [key: number]: (frame: number) => void
 }
 
 type OptionalProps = {
@@ -59,6 +60,7 @@ export class Animation implements Drawable, Updateable {
     const start = { x: props.width * props.firstFrame, y: 0 }
     const lastFrame = props.lastFrame || props.firstFrame
     const range = lastFrame - props.firstFrame + 1
+
     for (let i = 0; i < range; i += 1) {
       this.frames.push(
         new Sprite(host, {
@@ -72,14 +74,37 @@ export class Animation implements Drawable, Updateable {
     this.wrapFrames = (frame: number) => wrap(0, range - 1, frame)
   }
 
+  /**
+   * Attempts to call a callback for the current frame. Note that callbacks
+   * are not required for ANY frames, so if no callbacks are defined, this will
+   * simply do nothing.
+   */
+  private callCurrentFrameCallback(key: string | number) {
+    const cb = get(this, ['props', 'callbacks', `${key}`])
+    if (typeof cb === 'function') {
+      cb(key)
+    }
+  }
+
+  /**
+   * Advances the Animation to the next frame, or handles "end of animation"
+   * behavior if the last frame has been reached.
+   * This will also call any callbacks associated with the new current frame.
+   */
   private incrementFrame() {
     this.currentFrame = this.wrapFrames(this.currentFrame + 1)
     this.currentFrameTime = 0
+    this.callCurrentFrameCallback(this.currentFrame)
   }
 
+  /**
+   * Resets the Animation and starts playing from the first frame.
+   * Also triggers the special "start" frame callback when defined.
+   */
   public start() {
     this.currentFrame = 0
     this.currentFrameTime = 0
+    this.callCurrentFrameCallback('start')
   }
 
   public update(dt: number) {
@@ -91,6 +116,10 @@ export class Animation implements Drawable, Updateable {
     }
   }
 
+  /**
+   * Draws the Sprite associated with the current frame
+   * @param ctx
+   */
   public draw(ctx: CanvasRenderingContext2D) {
     this.frames[this.currentFrame].draw(ctx)
   }
