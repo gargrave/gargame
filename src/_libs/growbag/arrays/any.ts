3fe5781fdadde913507527660f745c7316b83b35
Gabe Hargrave <gkhargrave@gmail.com>
2019-07-20 11:04:23 -0700

    Add tests and cleanup any()

--- src/_libs/growbag/arrays/any.ts
+++ src/_libs/growbag/arrays/any.ts
@@ -1,10 +1,13 @@
-// TODO: need tests here
+/**
+ * Returns whether any values in `arr` return true for `predicate`.
+ * @param arr
+ * @param predicate
+ */
 export function any<T>(arr: T[], predicate: (T) => boolean): boolean {
   for (const value of arr) {
     if (predicate(value)) {
       return true
     }
   }
-
   return false
 }
