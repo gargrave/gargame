bd674629e9a495a557cbc114d823b5de8ae532c0
Gabe Hargrave <gkhargrave@gmail.com>
2019-07-20 11:53:47 -0700

    Deps upgrades and linting fixes

--- src/interfaces/Collider.ts
+++ src/interfaces/Collider.ts
@@ -1,8 +1,10 @@
-export interface Collider {
+export interface Collidable {
+  overlaps(other: Collidable): boolean
+}
+
+export interface Collider extends Collidable {
   readonly active: boolean
 
   activate(): void
   deactivate(): void
-
-  overlaps(other: any): boolean
 }
